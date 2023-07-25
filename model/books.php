4c0a476cbe1fa7bdb1e811c3fe1e71ca35a9f01b
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2023-07-25 13:31:26 -0500

    aa

--- model/books.php
+++ model/books.php
@@ -4,68 +4,7 @@ class books{
 
     protected $db;
 
-    
-    private $user_id; // Propiedad privada
-    private $firs_name; // Propiedad privada
-    private $last_name; // Contador privada 
-    private $cedula; // Contador privada
-    private $email; // Propiedad privada 
-    private $id; // Propiedad privada 
-
-    public function getid() // Método para obtener el valor
-    {
-        return $this->id;
-    }
-    public function getfirs_name() // Método para obtener
-    {
-        return $this->firs_name;
-    }
-
-    public function getlast_name()
-    {
-        return $this->last_name;
-    }
-
-    public function getcedula()
-    {
-        return $this->cedula;
-    }
-
-    public function getemail()
-    {
-        return $this->email;
-    }
-
-
     // -----------------------------------------------------------------
-
-
-    public function setid($id)
-    {
-        $this->id = $id;
-    }
-
-    public function setfirs_name($firs_name)
-    {
-        $this->firs_name = $firs_name;
-    }
-
-    public function setlast_name($last_name)
-    {
-        $this->last_name = $last_name;
-    }
-
-    public function setcedula($cedula)
-    {
-        $this->cedula = $cedula;
-    }
-
-    public function setemail($email)
-    {
-        $this->email = $email;
-    }
-
-
 
     // public function __construct(
     //     PDO $connection
@@ -91,16 +30,6 @@ class books{
         $stm -> bindValue(":id", $id);
         $stm -> execute();
         return $stm -> fetchAll();
-    }
-
-    function store(){
-        $sql = "NSERT INTO books (user_id, firs_name, last_name, cedula, email) VALUES (:user_id, :firs_name, :last_name, :cedula, :email)";
-        $stm = $this -> db -> prepare ($sql);
-        $stm -> bindValue(':user_id', $this -> user_id);
-        $stm -> bindValue(':firs_name', $this -> firs_name);
-        $stm -> bindValue(':last_name', $this -> last_name);
-        $stm -> bindValue(':cedula', $this -> cedula);
-        $stm -> bindValue(':email', $this -> email);
     }
 
 }
